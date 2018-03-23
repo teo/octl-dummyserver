8e5787b3a34e7d43e762642938ff9db7a606ab6c
Teo Mrnjavac <teo.mrnjavac@cern.ch>
2018-03-23 12:35:16 +0100

    Implement dummy gRPC server.

--- server.go
+++ server.go
@@ -23,7 +23,7 @@
  */
 
 //go:generate protoc --go_out=plugins=grpc:. protos/octlserver.proto
-package core
+package octl_dummyserver
 
 import (
   "golang.org/x/net/context"
@@ -33,25 +33,21 @@ import (
 	"google.golang.org/grpc/reflection"
 	"runtime"
 	"fmt"
-	"google.golang.org/grpc/status"
-	"google.golang.org/grpc/codes"
+	"time"
+	"github.com/pborman/uuid"
 )
 
 
 func NewServer() *grpc.Server {
 	s := grpc.NewServer()
-	pb.RegisterOctlServer(s, &RpcServer{
-	})
+	pb.RegisterOctlServer(s, &RpcServer{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	return s
 }
 
 func (m *RpcServer) logMethod() {
-	if !m.state.config.verbose {
-		return
-	}
-	pc, _, _, ok := runtime.Caller(0)
+	pc, _, _, ok := runtime.Caller(1)
 	if !ok {
 		return
 	}
@@ -59,52 +55,123 @@ func (m *RpcServer) logMethod() {
 	if fun == nil {
 		return
 	}
-	log.WithPrefix("rpcserver").
-		WithField("method", fun.Name()).
-		Debug("handling RPC request")
+	fmt.Printf("handling RPC request %s\n", fun.Name())
 }
 
 // Implements interface pb.OctlServer
-type RpcServer struct {
+type RpcServer struct {}
 
-}
+func (m *RpcServer) TrackStatus(req *pb.StatusRequest, statusServer pb.Octl_TrackStatusServer) error {
+	m.logMethod()
+	for {
+		err := statusServer.Send(&pb.StatusReply{
+			State: "ok",
+			StatusUpdates: []*pb.StatusUpdate{
+				{
+					Level: pb.StatusUpdate_INFO,
+					Event: &pb.StatusUpdate_GenericMessage{
+						GenericMessage: &pb.Event_GenericMessage{
+							Message: fmt.Sprintf("sending update at %s", time.Now().String()),
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			fmt.Printf(err.Error())
+			return err
+		} else {
+			fmt.Printf("stream update sent\n")
+		}
 
-func (*RpcServer) TrackStatus(*pb.StatusRequest, pb.Octl_TrackStatusServer) error {
-	panic("implement me")
+		time.Sleep(1000*time.Millisecond)
+	}
+	return nil
 }
 
 func (m *RpcServer) GetFrameworkInfo(context.Context, *pb.GetFrameworkInfoRequest) (*pb.GetFrameworkInfoReply, error) {
-	panic("implement me")
+	m.logMethod()
+	return &pb.GetFrameworkInfoReply{
+		State: "CONFIGURED",
+		FrameworkId: uuid.NewUUID().String(),
+		RolesCount: 42,
+		EnvironmentsCount: 6,
+	}, nil
 }
 
-func (*RpcServer) Teardown(context.Context, *pb.TeardownRequest) (*pb.TeardownReply, error) {
-	panic("implement me")
+func (m *RpcServer) Teardown(context.Context, *pb.TeardownRequest) (*pb.TeardownReply, error) {
+	m.logMethod()
+	return &pb.TeardownReply{}, nil
 }
 
 func (m *RpcServer) GetEnvironments(context.Context, *pb.GetEnvironmentsRequest) (*pb.GetEnvironmentsReply, error) {
-	panic("implement me")
+	m.logMethod()
+	return &pb.GetEnvironmentsReply{
+		FrameworkId: uuid.NewUUID().String(),
+		Environments: []*pb.EnvironmentInfo{
+			{
+				State: "CONFIGURED",
+				Roles: []string{"flp1", "flp2", "epn3"},
+				Id: uuid.NewUUID().String(),
+				CreatedWhen: time.Now().String(),
+			},
+			{
+				State: "RUNNING",
+				Roles: []string{"flp3", "flp4", "epn1", "epn2"},
+				Id: uuid.NewUUID().String(),
+				CreatedWhen: time.Now().String(),
+			},
+		},
+	}, nil
 }
 
 func (m *RpcServer) NewEnvironment(cxt context.Context, request *pb.NewEnvironmentRequest) (*pb.NewEnvironmentReply, error) {
-	panic("implement me")
+	m.logMethod()
+	return &pb.NewEnvironmentReply{
+		Id: uuid.NewUUID().String(),
+		State: "CONFIGURED",
+	}, nil
 }
 
-func (*RpcServer) GetEnvironment(context.Context, *pb.GetEnvironmentRequest) (*pb.GetEnvironmentReply, error) {
-	panic("implement me")
+func (m *RpcServer) GetEnvironment(context.Context, *pb.GetEnvironmentRequest) (*pb.GetEnvironmentReply, error) {
+	m.logMethod()
+	return &pb.GetEnvironmentReply{
+		Environment: &pb.EnvironmentInfo{
+			State: "CONFIGURED",
+			Roles: []string{"flp1", "flp2", "epn3"},
+			Id: uuid.NewUUID().String(),
+			CreatedWhen: time.Now().String(),
+		},
+	}, nil
 }
 
-func (*RpcServer) ControlEnvironment(context.Context, *pb.ControlEnvironmentRequest) (*pb.ControlEnvironmentReply, error) {
-	panic("implement me")
+func (m *RpcServer) ControlEnvironment(context.Context, *pb.ControlEnvironmentRequest) (*pb.ControlEnvironmentReply, error) {
+	m.logMethod()
+	return &pb.ControlEnvironmentReply{}, nil
 }
 
-func (*RpcServer) ModifyEnvironment(context.Context, *pb.ModifyEnvironmentRequest) (*pb.ModifyEnvironmentReply, error) {
-	panic("implement me")
+func (m *RpcServer) ModifyEnvironment(context.Context, *pb.ModifyEnvironmentRequest) (*pb.ModifyEnvironmentReply, error) {
+	m.logMethod()
+	return &pb.ModifyEnvironmentReply{
+		FailedOperations: nil,
+	}, nil
 }
 
-func (*RpcServer) DestroyEnvironment(context.Context, *pb.DestroyEnvironmentRequest) (*pb.DestroyEnvironmentReply, error) {
-	panic("implement me")
+func (m *RpcServer) DestroyEnvironment(context.Context, *pb.DestroyEnvironmentRequest) (*pb.DestroyEnvironmentReply, error) {
+	m.logMethod()
+	return &pb.DestroyEnvironmentReply{}, nil
 }
 
-func (*RpcServer) GetRoles(context.Context, *pb.GetRolesRequest) (*pb.GetRolesReply, error) {
-	panic("implement me")
+func (m *RpcServer) GetRoles(context.Context, *pb.GetRolesRequest) (*pb.GetRolesReply, error) {
+	m.logMethod()
+	return &pb.GetRolesReply{
+		Roles: []*pb.RoleInfo{
+			{
+				Name: "flp1",
+				State: "RUNNING",
+				Condition: "ACTIVE",
+				Hostname: "flp1.cern.ch",
+			},
+		},
+	}, nil
 }
